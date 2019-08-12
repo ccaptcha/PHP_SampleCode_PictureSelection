552cc24e229082de50a3df5ac1b285c3859d8f22
ccaptcha <51909591+ccaptcha@users.noreply.github.com>
2019-08-12 12:41:05 +0430

    Create index.php
    Sample Code of PHP PictureSelection

---
+++ index.php
@@ -0,0 +1,43 @@
+﻿<head>
+    <meta charset="utf-8" />
+    <title>CCAPTCHA Demo in PHP</title>  
+    <script src=" https://widget.ccaptcha.com/js/ccaptcha_version2_M9.js"></script>
+</head>
+
+<body>
+
+<form method="post">
+<div  class="ccaptcha_M9" id="ccaptcha_M9"  name="Ccaptcha_M9" data-ccaptcha_apicode="Type Your API Code Here"></div>
+<input type="submit" name="test" id="test" value="Validate" /><br/>
+
+<?php
+if(array_key_exists('test',$_POST)){
+
+//Type Your SecretCode Here
+$Secret_Code="Your Secret Code";
+$url = ' https://api.ccaptcha.com/api/Validate9/ValidationPost;
+$myvars = 'Token=' . $_POST['ccaptcha_token_input'] . '&Secret_Code=' . $Secret_Code;
+$ch = curl_init( $url );
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+$response = curl_exec( $ch );
+    if($response == '"true"')
+    {
+       //CCAPTCHA SOLVED!
+    echo 'true';
+    }
+    else
+    {
+     //CCAPTCHA Not Solved!
+     echo 'false';
+    }
+
+}
+?>
+</form>
+
+</body>
